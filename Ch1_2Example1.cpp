604cc3729c9a9e0506cb92011b74eb2d5d7b18e9
Mohammad Rokibur RAhman <rokib.rahman@gmail.com>
2017-04-26 16:41:18 -0400

    Create Ch1_2Example1.cpp

---
+++ Ch1_2Example1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	short int i = 2;
+	int j= -5;
+	int bob = 1234;
+	cout << "short int i: " << i << "\nint j: " << j << "\nint bob: " << bob << endl;
+
+	char grade;
+	cout << "Enter the students exam grade: ";
+	cin >> grade;
+	cout << "The student XYZ received an: " << grade << endl;
+
+	float l = 2.3452f;
+	double m;
+	m = 20.02;
+	long double n = 8.99999999999;
+	cout << "float l: " << l << "\ndouble m: " << m << "\nlong double n: " << n << endl;
+
+	double x = 8.99999999999;
+	cout << "double x: " << x << endl;
+
+	cout << "" << endl;
+	cout << "  HELLO WORLD" << endl;
+	cout << "" << endl;
+
+	system ("pause");
+
+	return 0;
+
+}
